eca57514add1e18c001e26188f3119f80a4ff4b4
ranjeetxsingh <ranjeet.fb12@gmail.com>
2024-08-18 20:54:30 +0530

    change title

--- components/utility/Page.tsx
+++ components/utility/Page.tsx
@@ -7,8 +7,8 @@ import React, { ReactChildren } from "react";
 function Page({ currentPage, meta: { title, desc }, children }: PageProps) {
   const pageTitle = `${
     currentPage === "Home"
-      ? "Brayden Wright - Web Developer, Designer, Creator."
-      : `${currentPage} - BraydenTW.io`
+      ? "Saranjeet - Graphic Designer"
+      : `${currentPage} - Saranjeet`
   }`;
   console.log(currentPage);
   return (
