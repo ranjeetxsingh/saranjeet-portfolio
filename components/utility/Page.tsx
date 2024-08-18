d65662e6bca3aa23ccb5d6fc0e4c9a38ca9def8a
ranjeetxsingh <ranjeet.fb12@gmail.com>
2024-08-18 21:36:26 +0530

    minor logo change

--- components/utility/Page.tsx
+++ components/utility/Page.tsx
@@ -46,7 +46,7 @@ function Page({ currentPage, meta: { title, desc }, children }: PageProps) {
         <meta property="og:description" content={desc} />
         <meta
           property="og:image"
-          content="https://braydentw.io/static/logos/icon_logo.svg"
+          content="https://braydentw.io/static/logos/icon_logo.svg" 
         />
 
         <meta property="twitter:card" content="summary_large_image" />
